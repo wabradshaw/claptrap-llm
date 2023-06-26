de87f11806524ad1cf160b899319e4de2be7a502
William Bradshaw <w.a.bradshaw@gmail.com>
2023-06-26 13:56:37 +0100

    First basic pass

--- app.py
+++ app.py
@@ -3,33 +3,60 @@ import os
 import openai
 from flask import Flask, redirect, render_template, request, url_for
 
+JOKE_SYSTEM = """You are a comedian that tells jokes in the following format:
+
+<A, B, C, D, E>
+S
+E!
+
+Where each symbol has the following meaning:
+A -> A random word or phrase of at least 8 letters.
+B -> The word that A either starts or ends with. 
+C -> A random word that either sounds like B or rhymes with B. The length of B and C should either be the same, or differ by 1 character.
+D -> A new word made by substituting C for D in A.
+S -> The setup. It should describe a combination of A and C.
+
+Here are some examples:
+
+<nightmare, night, fight, fightmare>
+What do you call a cross between a bad dream and a battle?
+A fightmare!
+
+<Cupcake, cup, pup, pupcake>
+What dog is made in a bakery?
+A pupcake!
+
+<Guitar, tar, star, guistar>
+What do you call a musical instrument that shines like a celestial body?
+A guistar!
+
+<Wonderful, won, run, runderful>
+What do you call an amazing race?
+Runderful!"""
+
+GENERIC_JOKE_PROMPT = "Tell a joke"
+
+MODEL = "gpt-3.5-turbo"
+
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
-
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+        response = openai.ChatCompletion.create(            
+            model=MODEL,
+            messages=[
+                {"role": "system", "content": JOKE_SYSTEM},
+                {"role": "user", "content": GENERIC_JOKE_PROMPT}
+            ],            
+            temperature=1.5
         )
-        return redirect(url_for("index", result=response.choices[0].text))
+        joke = response.choices[0].message.content
+        return redirect(url_for("index", result=joke))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
